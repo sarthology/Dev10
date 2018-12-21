1b8c23ab3051006bd9b987f1b00cbb4e8312a16d
Sarthak Sharma <7lovesharma7@gmail.com>
2018-12-21 12:53:53 +0530

    Logo changed

--- main.js
+++ main.js
@@ -1,12 +1,10 @@
 var menubar = require('menubar');
 var path = require('path');
-const {
-    ipcMain
-} = require('electron');
+const {ipcMain} = require('electron');
 
 var mb = menubar({
     index: "file://" + path.join(__dirname, '/app/index.html'),
-    icon: path.join(__dirname, '/icons/logo.png'),
+    icon: path.join(__dirname, '/icons/logoTemplate.png'),
     width: 360,
     height: 500,
     tooltip: "dev 10",
