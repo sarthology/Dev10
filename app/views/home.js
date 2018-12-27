d0baa2df6c9a054016409252a5e19c44ba7d30a1
Basti Ortiz <ortizbasti16@gmail.com>
2018-12-27 16:03:19 +0800

    Put Handlebars into files

--- app/views/home.js
+++ app/views/home.js
@@ -1,63 +1,22 @@
 'use strict';
 
-// Global Variables
-const home = {};
+// NATIVE IMPORTS
+const path = require('path');
 
-home.body = `<div class="tags" id="tags" style="min-height:35px">    
-        </div>
-        <div id="post-list">
-            <div class="loader">
-                <img src="../icons/loader-2.gif">
-            </div>
-        </div>`;
+// MODULE IMPORTS
+const readFileSync = require('../util/readFileSync');
 
-home.post =`{{#each posts}}
-    <div class="post" onclick="openLink('{{link}}')">
-        <div class="post-info">
-            <div class="author">
-                <img src="{{authorImage}}" alt="">
-            </div>
-            <div>
-                <p class="title">{{title}}</p>
-                <p class="author-name">{{author}} </p>
-            </div>
-        </div>
-        <div class="action-section">
-            <div class="tags">
-                {{#each tags}}
-                    {{#if backgroundColor}}
-                        <div class="tag" style="{{backgroundColor}}{{color}}" onclick="getTag(event,'{{name}}')">
-                    {{else}}
-                        <div class="tag" onclick="getTag(event,'{{name}}')">
-                    {{/if}}
-                            <p>{{name}}</p>
-                        </div>
-                {{/each}}
-            </div>
-            <div class="save-to-piggy">
-                {{#if saved}}
-                    <div class="pig-true" onclick="savePost(event,'{{@index}}')">
-                {{else}}
-                    <div class="pig-false" onclick="savePost(event,'{{@index}}')">
-                {{/if}}
-                    </div>
-            </div>
-        </div>
-    </div>   
-{{/each}}`;
-
-home.tag =`{{#each tags}}
-{{#if backgroundColor}}
-    <div class="tag" style="{{backgroundColor}}{{color}}" onclick="getTag(event,'{{name}}')">
-{{else}}
-    <div class="tag" onclick="getTag(event,'{{name}}')">
-{{/if}}
-        <p>{{name}}</p>
-    </div>  
-{{/each}}`;
-
-home.loader = `<div class="loader">
-<img src="../icons/loader-2.gif">
-</div>`;
-
-module.exports = home;
+module.exports = {
+    body: readFileSync(
+        path.resolve(__dirname, './home-body.hbs')
+    ),
+    post: readFileSync(
+        path.resolve(__dirname, './home-post.hbs')
+    ),
+    tag: readFileSync(
+        path.resolve(__dirname, './home-tag.hbs')
+    ),
+    loader: readFileSync(
+        path.resolve(__dirname, './home-loader.hbs')
+    )
+};
--- app/views/piggyList.js
+++ app/views/piggyList.js
@@ -1,18 +1,11 @@
 'use strict';
 
-// Global Variables
-const piggyList= {};
+// NATIVE IMPORTS
+const path = require('path');
 
-piggyList.body = `<div class="piggy-list">
-        <img src="../icons/pig.png" alt="">
-        <h2>Your Piggy List</h2>
-        </div>
-        <div class="tags piggy-tags" id="tags" style="min-height:35px">    
-        </div>
-        <div id="post-list">
-            <div class="loader">
-                <img src="../icons/loader-2.gif">
-            </div>
-        </div>`;
+// MODULE IMPORTS
+const readFileSync = require('../util/readFileSync');
 
-module.exports = piggyList;
+module.exports = readFileSync(
+    path.resolve(__dirname, './piggyList.hbs')
+);
